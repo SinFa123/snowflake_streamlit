635605bf82ae87381e4e11c72726e91d4f028c25
SinFa123 <41826066+SinFa123@users.noreply.github.com>
2023-11-08 23:14:38 +0000

    Update streamlit_app.py
    Filter dataframe by user choices

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,8 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index("Fruit")
 
 ## Enable user to select fruits
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
+fruits_to_show = my_fruits_list.loc[fruits_selected]
 
-streamlit.dataframe(my_fruit_list)
+#display the table on the page
+streamlit.dataframe(fruits_to_show)
