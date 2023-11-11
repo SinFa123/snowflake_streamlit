94c0feaf7ad88c80bee1c2037c8733c46024d64c
SinFa123 <41826066+SinFa123@users.noreply.github.com>
2023-11-11 15:28:03 +0000

    Update streamlit_app.py
    Added a bunch of fruityvice stuff to it

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,35 @@
-import streamlit
-import pandas
+# import streamlit
+# import pandas
 
-streamlit.title('My Parents New Healthy Diner')
+# streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Favorites')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥣 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+# streamlit.header('Breakfast Favorites')
+# streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+# streamlit.text('🥣 Kale, Spinach & Rocket Smoothie')
+# streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+# streamlit.text('🥑🍞 Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+# streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index("Fruit")
+# my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# my_fruit_list = my_fruit_list.set_index("Fruit")
 
-## Enable user to select fruits
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
+# ## Enable user to select fruits
+# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
 
-## Fix exception
-try:
-    fruits_to_show = my_fruit_list.loc[fruits_selected]
-except KeyError:
-    streamlit.error("One of the selected fruits is not available.")
+# ## Fix exception
+# try:
+#     fruits_to_show = my_fruit_list.loc[fruits_selected]
+# except KeyError:
+#     streamlit.error("One of the selected fruits is not available.")
 
 
-#display the table on the page
-streamlit.dataframe(fruits_to_show)
+# #display the table on the page
+# streamlit.dataframe(fruits_to_show)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+streamlit.header("Fruityvice Fruit Advice!")
+
+streamlit.text(fruityvice_response.json())
