74c6f8418c5b5dfafe97dbdf499a1c8bc2c5cfff
SinFa123 <41826066+SinFa123@users.noreply.github.com>
2023-11-11 15:42:03 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,4 +34,4 @@ fruityvice_value = "kiwi"
 fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruityvice_value}")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-streamlist.dataframe(fruityvice_normalized)
+streamlist.dataframe(fruits_to_show)
